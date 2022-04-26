dc15ca03e7ff64ac0829c7f90f949e280b510f20
Flavio <flavioantonio5@hotmail.com>
2022-04-26 20:11:03 -0300

    Initial

--- loja/settings.py
+++ loja/settings.py
@@ -122,6 +122,9 @@ STATCIFILES_DIRS = [
     os.path.join('static')
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
